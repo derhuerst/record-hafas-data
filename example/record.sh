469cae90e6dc7261c68f8c8211b24310865c5db9
Jannis R <mail@jannisr.de>
2020-03-01 14:06:18 +0100

    example :memo:

--- example/record.sh
+++ example/record.sh
@@ -2,4 +2,12 @@
 set -e
 set -o pipefail
 
-# todo
+cd $(dirname $0)
+
+# ./node_modules/.bin/monitor-hafas ./vbb-hafas.js departure,stopover \
+# 	stations 900000100001,900000100003
+# ./node_modules/.bin/monitor-hafas ./vbb-hafas.js stopover,position \
+# 	bbox 52.6 13.3 52.3 13.6
+
+rm -rf vbb-deps.ldb
+cat ./vbb-deps.ndjson | ../record.js vbb-deps.ldb
---
+++ example/vbb-hafas.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const createVbbHafas = require('vbb-hafas')
+
+const vbbHafas = createVbbHafas('record-hafas-data example')
+
+module.exports = vbbHafas
